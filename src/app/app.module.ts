f12b835c2295e4e51e5ce10c2e4d209707cb1bc9
Jyoti Deshmukh <test8600896492@gmail.com>
2021-12-11 22:11:03 +0530

    added basic route to app and reuired components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,34 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainContainerComponent } from './main-container/main-container.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ListProfileComponent } from './list-profile/list-profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -18,13 +47,19 @@ import { ListProfileComponent } from './list-profile/list-profile.component';
     SidebarComponent,
     MainContainerComponent,
     ProfileComponent,
-    ListProfileComponent
+    ListProfileComponent,
+    DashboardComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {
+      enableTracing: true,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
